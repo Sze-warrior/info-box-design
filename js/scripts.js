444188c50f1937d98ab6d23dda944797332c83f2
Michael Szeto <szetomj@gmail.com>
2017-08-01 22:15:14 +1200

    made toggling interaction for close and open

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 var closeBox = document.querySelectorAll(".close-icon-container");
-var showBox = document.querySelectorAll(".show-info-box");
-	
-if(closeBox){
+var showBox = document.querySelectorAll(".show-info");
 
+if(closeBox){
 	function closeInfoBox(e){
 		var target = e.currentTarget.parentNode;
 		Velocity(target, "slideUp", {duration: 200, easing:"ease-in-out"});
+		Velocity(showBox, {opacity: 1}, {display: "block", duration:200, delay:210 });
 	};
-
-
-	for(var i = 0; i < closeBox.length; i++){
-		closeBox[i].addEventListener("click", closeInfoBox, false);
-	}
-
 };
+
+if(showBox){
+	function showInfoBox(e){
+		var target = e.currentTarget.nextElementSibling;
+		Velocity(showBox, "slideDown", { opacity: 0, display: "none", duration:1});
+		Velocity(target, "slideDown", {duration: 250, easing:"ease-in-out"});
+	};
+};
+
+for(var i = 0; i < closeBox.length; i++){
+	closeBox[i].addEventListener("click", closeInfoBox, false);
+}
+
+for(var i = 0; i < showBox.length; i++){
+	showBox[i].addEventListener("click", showInfoBox, false);
+}	
