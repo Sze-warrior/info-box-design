b16e99f4d29092da670682f1f21543fb04b69fd5
Michael Szeto <szetomj@gmail.com>
2017-08-03 21:59:15 +1200

    Redid javaScript to select correctly and with classes
    Redid javaScript to select correctly and with classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
-var closeBox = document.querySelectorAll(".close-icon-container");
-var showBox = document.querySelectorAll(".show-info");
+var closeInfoBox = document.querySelectorAll(".cross-icon-container");
+var showBox = document.querySelectorAll(".show-info-box");
 
-if(closeBox){
-	function closeInfoBox(e){
-		var target = e.currentTarget.parentNode;
-		Velocity(target, "slideUp", {duration: 200, easing:"ease-in-out"});
-		Velocity(showBox, {opacity: 1}, {display: "block", duration:200, delay:210 });
+if(closeInfoBox){
+
+	function closeBox(e){
+		var target = e.currentTarget;//selects current target as the cross icon clicked
+		var targetParent = target.parentNode;//travels up to the info-box class as its parent
+		var targetGrandparent = targetParent.parentNode;//travels up to the info-box-container class as its grandparent
+		var showBox = targetGrandparent.children[0];//Selects its first child in this case the show-info-box class
+
+		targetParent.classList.add("hide");
+		targetGrandparent.classList.add("no-border");
+		showBox.classList.add("show");
 	};
 
-	for(var i = 0; i < closeBox.length; i++){
-		closeBox[i].addEventListener("click", closeInfoBox, false);
-	};
+	for(var i = 0; i < closeInfoBox.length; i++){
+		closeInfoBox[i].addEventListener("click", closeBox, false);
+	}
 
 };
+
 
 if(showBox){
-	function showInfoBox(e){
-		var target = e.currentTarget.nextElementSibling;
-		Velocity(showBox, "slideDown", { opacity: 0, display: "none", duration:1});
-		Velocity(target, "slideDown", {duration: 250, easing:"ease-in-out"});
+
+	function openBox(e){
+		var target = e.currentTarget;//selects current target as the show-info-box clicked
+		var targetParent = target.parentNode;//travels up to the how-info-box as its parent
+		var showInfo = targetParent.children[1];//Selects its second child in this case the info-box class
+
+		targetParent.classList.remove("no-border");
+		showInfo.classList.remove("hide");
+		e.currentTarget.classList.remove("show");
 	};
 
-
 	for(var i = 0; i < showBox.length; i++){
-		showBox[i].addEventListener("click", showInfoBox, false);
+		showBox[i].addEventListener("click", openBox, false);
 	}
-};
-	
+
+}
