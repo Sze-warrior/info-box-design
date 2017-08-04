0670abde1d5271eac23bc545f4807271a42d2649
Michael Szeto <szetomj@gmail.com>
2017-08-04 21:51:52 +1200

    Restructured mark up and JS to be smaller code
    Restructured mark up and JS to be smaller code

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,33 @@
-var closeInfoBox = document.querySelectorAll(".cross-icon-container");
-var showBox = document.querySelectorAll(".show-info-box");
+var closeInfoBox = document.querySelectorAll(".info-box");
+var showMore = document.querySelectorAll(".show-info-box");
 
-if(closeInfoBox){
+if(closeInfoBox || showMore){
 
 	function closeBox(e){
 		var target = e.currentTarget;//selects current target as the cross icon clicked
-		var targetParent = target.parentNode;//travels up to the info-box class as its parent
-		var targetGrandparent = targetParent.parentNode;//travels up to the info-box-container class as its grandparent
-		var showBox = targetGrandparent.children[0];//Selects its first child in this case the show-info-box class
+		var targetParent = target.parentNode;
+		var openInfoBox = targetParent.children[0];
 
-		targetParent.classList.add("hide");
-		targetGrandparent.classList.add("no-border");
-		showBox.classList.add("show");
+		target.classList.add("hide");
+		openInfoBox.classList.add("show");
 	};
+
+	function showInfoBox(e){
+		var target = e.currentTarget;//selects current target as the show me more link clicked
+		var targetParent = target.parentNode;
+		var openInfoBox = targetParent.children[1];
+
+		openInfoBox.classList.remove("hide");
+		target.classList.remove("show");
+	};	
+
 
 	for(var i = 0; i < closeInfoBox.length; i++){
 		closeInfoBox[i].addEventListener("click", closeBox, false);
 	}
 
-};
-
-
-if(showBox){
-
-	function openBox(e){
-		var target = e.currentTarget;//selects current target as the show-info-box clicked
-		var targetParent = target.parentNode;//travels up to the how-info-box as its parent
-		var showInfo = targetParent.children[1];//Selects its second child in this case the info-box class
-
-		targetParent.classList.remove("no-border");
-		showInfo.classList.remove("hide");
-		e.currentTarget.classList.remove("show");
-	};
-
-	for(var i = 0; i < showBox.length; i++){
-		showBox[i].addEventListener("click", openBox, false);
+	for(var i = 0; i < showMore.length; i++){
+		showMore[i].addEventListener("click", showInfoBox, false);
 	}
 
-}
+};
